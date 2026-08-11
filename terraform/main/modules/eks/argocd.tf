dc25389388e224ba8b1498098b8b96c5cb414788
ho viet vinh <hovietvinh29032004@gmail.com>
2026-08-11 15:08:34 +0700

    fix tf

--- terraform/main/modules/eks/argocd.tf
+++ terraform/main/modules/eks/argocd.tf
@@ -10,27 +10,3 @@ resource "helm_release" "argocd" {
     aws_eks_node_group.eks_node_group
   ]
 }
-
-data "aws_secretsmanager_secret" "argocd_github_oauth" {
-  name = "${var.project_name}-${var.env}-argocd-github-oauth"
-}
-
-data "aws_secretsmanager_secret_version" "argocd_github_oauth" {
-  secret_id = data.aws_secretsmanager_secret.argocd_github_oauth.id
-}
-
-locals {
-  argocd_github_oauth = jsondecode(data.aws_secretsmanager_secret_version.argocd_github_oauth.secret_string)
-}
-
-resource "kubernetes_secret" "argocd_github_oauth" {
-  metadata {
-    name      = "argocd-github-oauth"
-    namespace = "argocd"
-  }
-  data = {
-    client_id     = local.argocd_github_oauth.client_id
-    client_secret = local.argocd_github_oauth.client_secret
-  }
-  depends_on = [helm_release.argocd]
-}
